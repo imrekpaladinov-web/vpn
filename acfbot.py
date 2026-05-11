99ccf78592d265069a2bdd9ee13845d2ac3f0464
imrekpaladinov-web <kontaktykanov@gmail.com>
2026-05-11 17:18:45 +0300

    Update acfbot.py

--- acfbot.py
+++ acfbot.py
@@ -4,8 +4,11 @@ from aiogram.filters import CommandStart
 
 import asyncio
 import os
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
+
+KAGGLE_API = os.getenv("KAGGLE_API")
 
 bot = Bot(token=TOKEN)
 
@@ -19,7 +22,33 @@ async def start(message: Message):
         "Задай мне любой вопрос и я отвечу. 🎉"
     )
 
+@dp.message()
+async def ai(message: Message):
+
+    try:
+
+        r = requests.post(
+            f"{KAGGLE_API}/generate",
+            json={
+                "text": message.text
+            },
+            timeout=120
+        )
+
+        data = r.json()
+
+        answer = data["response"]
+
+    except Exception as e:
+
+        answer = f"Ошибка: {e}"
+
+    await message.answer(answer[:4000])
+
 async def main():
+
+    print("BOT STARTED")
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
